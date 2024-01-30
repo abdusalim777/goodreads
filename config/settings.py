a5218cc91904ee62e521797426c43089448b1528
abdusalim777 <150586395+abdusalim777@users.noreply.github.com>
2024-01-30 11:35:33 +0500

    final

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ LOGIN_URL = 'users:login'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['goodreads.uz', 'www.goodreads.uz', '127.0.0.1']
+ALLOWED_HOSTS = ['goodreads-website.uz', 'www.goodreads-website.uz', '127.0.0.1']
 
 # Application definition
 
@@ -121,14 +121,14 @@ USE_TZ = True
 AUTH_USER_MODEL = 'users.CustomUser'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/goodread/goodreads.uz/django/media-files'
+MEDIA_ROOT = '/home/goodread/goodreads-website.uz/django/media-files'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/goodread/goodreads.uz/django/staticfiles'
-STATICFILES_DIRS = ('/home/goodread/goodreads.uz/django/static',)
+STATIC_ROOT = '/home/goodread/goodreads-website.uz/django/staticfiles'
+STATICFILES_DIRS = ('/home/goodread/goodreads-website.uz/django/static',)
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static'
 # ]
