8b1d83ad79860c5d31e54225a41b7436d4fbc756
abdusalim777 <150586395+abdusalim777@users.noreply.github.com>
2024-01-31 21:11:29 +0500

    final

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,9 @@ urlpatterns = [
     path('api/', include('api.urls')),
 
 ]
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
